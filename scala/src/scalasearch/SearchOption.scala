2fbc9e99afe7bd98488a278376f24b9cea6f7115
Cary Clark <clarkcb@gmail.com>
2012-07-16 22:59:59 -0700

    Removed unneeded SearchOption.scala

--- scala/src/scalasearch/SearchOption.scala
+++
@@ -1,7 +0,0 @@
-package scalasearch
-
-class SearchOption(val shortarg: String, val longarg: String, val func: Function, val desc: String) {
-    override def toString() = {
-        "SearchOption(shortarg: " + shortarg + ", longarg: " + longarg + ")"
-    }
-}
