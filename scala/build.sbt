a2b2cf58c4374df9f9789c3db1696478ec02a6fd
Cary Clark <clarkcb@gmail.com>
2012-08-17 21:48:14 -0700

    Added sbt build file, and a unit test class for SearchOptions

---
+++ scala/build.sbt
@@ -0,0 +1,10 @@
+name := "scalasearch"
+
+version := "1.0"
+
+scalaVersion := "2.9.2"
+
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.8.1" % "test",
+  "org.scalatest" %% "scalatest" % "1.8" % "test"
+)
---
+++ scala/src/test/scala/scalasearch/SearchOptionsTest.scala
@@ -0,0 +1,100 @@
+package scalasearch
+
+import java.util.regex.Pattern
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SearchOptionsTest extends FunSuite with BeforeAndAfterAll{
+
+  val startpath = "."
+  val searchString = "Search"
+  val requiredArgs = List("-s", searchString, startpath)
+
+  test("print argOptions") {
+    SearchOptions.argOptions.foreach(s => println(s.toString))
+    SearchOptions.flagOptions.foreach(s => println(s.toString))
+
+  }
+
+  test("test getMapFromOptions") {
+    val argMap = SearchOptions.mapFromOptions(SearchOptions.argOptions)
+    println("argMap:")
+    println(argMap.toString)
+
+    val flagMap = SearchOptions.mapFromOptions(SearchOptions.flagOptions)
+    println("flagMap:")
+    println(flagMap.toString)
+  }
+
+  def assertDefaultSettings(settings:SearchSettings) = {
+    val defaultSettings = new SearchSettings()
+    assert(settings.casesensitive == defaultSettings.casesensitive)
+    assert(settings.debug == defaultSettings.debug)
+    assert(settings.dotiming == defaultSettings.dotiming)
+    assert(settings.firstmatch == defaultSettings.firstmatch)
+    assert(settings.listfiles == defaultSettings.listfiles)
+    assert(settings.listlines == defaultSettings.listlines)
+    assert(settings.multilinesearch == defaultSettings.multilinesearch)
+    assert(settings.printresults == defaultSettings.printresults)
+    assert(settings.printusage == defaultSettings.printusage)
+    assert(settings.printversion == defaultSettings.printversion)
+    assert(settings.searchcompressed == defaultSettings.searchcompressed)
+    assert(settings.verbose == defaultSettings.verbose)
+  }
+
+  test("""test settingsFromArgs with requiredArgs""") {
+    val args = requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    println("settings.startpath: "+settings.startpath)
+    assertDefaultSettings(settings)
+    assert(settings.startpath == startpath)
+    assert(settings.searchPatterns.size == 1)
+    assert(settings.searchPatterns.toArray()(0).toString == searchString)
+  }
+
+  test("""test settingsFromArgs with args="-x scala ." """) {
+    val args = List("-x", "scala") ::: requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    println("settings.inExtensions: "+settings.inExtensions)
+    assert(settings.inExtensions.size() == 1)
+    assert(settings.inExtensions.toArray()(0) == "scala")
+  }
+
+  test("""test settingsFromArgs with args="-X scala ." """) {
+    val args = List("-X", "scala") ::: requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    println("settings.outExtensions: "+settings.outExtensions)
+    assert(settings.outExtensions.size() == 1)
+    assert(settings.outExtensions.toArray()(0) == "scala")
+  }
+
+  test("""test settingsFromArgs with args="-1 ." """) {
+    val args = List("-1") ::: requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    assert(settings.firstmatch == true)
+  }
+
+  test("""test settingsFromArgs with args="-d scalasearch ." """) {
+    val args = List("-d", "scalasearch") ::: requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    println("settings.inDirPatterns: "+settings.inDirPatterns)
+    assert(settings.inDirPatterns.size() == 1)
+    assert(settings.inDirPatterns.toArray()(0).toString == "scalasearch")
+  }
+
+  test("""test settingsFromArgs with args="-f Search ." """) {
+    val args = List("-f", "Search") ::: requiredArgs
+    println("args: "+args)
+    val settings = SearchOptions.settingsFromArgs(args)
+    println("settings.inFilePatterns: "+settings.inFilePatterns)
+    assert(settings.inFilePatterns.size() == 1)
+    assert(settings.inFilePatterns.toArray()(0).toString == "Search")
+  }
+}
