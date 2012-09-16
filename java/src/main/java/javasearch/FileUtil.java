019b4b377313ed143b0ef24309e6111fbd332f32
Cary Clark <clarkcb@gmail.com>
2012-09-15 22:51:26 -0700

    Switched FileUtil to get file info from filetypes.xml for java

--- java/src/main/java/javasearch/FileUtil.java
+++ java/src/main/java/javasearch/FileUtil.java
@@ -11,61 +11,67 @@ Utility class to determine file types, etc.
 package javasearch;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class FileUtil {
 
-	private static Set<String> _binaryExtensions =
-		new HashSet<String>(
-			Arrays.asList(
-				("ai bin class com dat dbmdl dcr dir dll dxr dms doc docx dot " +
-				 "exe hlp indd lnk mo obj pdb ppt psd pyc pyo qxd so swf sys " +
-				 "vsd xls xlsx xlt").split("\\s+")
-			)
-		);
-	private static Set<String> _compressedExtensions =
-		new HashSet<String>(
-			Arrays.asList(
-				("bz2 cpio ear gz hqx jar pax rar sit sitx tar tgz war " +
-				 "zip Z").split("\\s+")
-			)
-		);
-	private static Set<String> _noSearchExtensions =
-		new HashSet<String>(
-			Arrays.asList(
-				("aif aifc aiff au avi bmp cab dat db dmg eps gif ico idlk ief " +
-				 "iso jpe jpeg jpg m3u m4a m4p mov movie mp3 mp4 mpe mpeg mpg " +
-				 "mxu ogg pdf pict png ps qt ra ram rm rpm scc snd suo tif tiff " +
-				 "wav").split("\\s+")
-			)
-		);
-	private static Set<String> _textExtensions =
-		new HashSet<String>(
-			Arrays.asList(
-				("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 " +
-				 "am app as asc ascx asm asp aspx bash bat bdsproj bsh c cc cfg " +
-				 "clj cls cmd cnt conf config cpp cs csh cshtml csproj css csv " +
-				 "ctl dat dbproj dbml dbschema ddl dep dfm disco dlg dof dpr dsp " +
-				 "dsw dtd env etx exp fls fs fsproj h hpp htm html ics iml in " +
-				 "inc ini ipr iws java js jsp layout log mak map master mht mxml " +
-				 "pas php php3 pl plist pm po properties py rb rc rc2 rdf resx " +
-				 "rex rtf rtx scala scc sgm sgml sh sln smi smil spec sqc sql st " +
-				 "str strings suml svg sxw t tcl tld tmx tsv txt url user vb " +
-				 "vbproj vbs vcf vcproj vdproj vm vrml vssscc vxml wbxml webinfo " +
-				 "wml wmls wrl wsd wsdd wsdl xlf xml xsd xsl xslt").split("\\s+")
-			)
-		);
-	private static Set<String> _unknownExtensions =
-		new HashSet<String>(
-			Arrays.asList(
-				("adm aps cli clw def df2 ncb nt nt2 orig pc plg roff sun t tex " +
-				 "texinfo tr xwd").split("\\s+")
-			)
-		);
+	// TODO: move to config file
+	private static final String fileTypePath = "/Users/cary/src/git/xsearch/shared/filetypes.xml";
+    private Map<String, Set<String>> fileTypeMap;
 
-	public FileUtil() {}
+    private Map<String,Set<String>> getFileTypeMap() {
+        Map fileTypeMap = new HashMap<String, Set<String>>();
+
+        File fileTypeFile = new File(fileTypePath);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(fileTypeFile);
+            doc.getDocumentElement().normalize();
+            NodeList filetypeNodes = doc.getElementsByTagName("filetype");
+            for (int i = 0; i < filetypeNodes.getLength(); i++) {
+                Node fileTypeNode = filetypeNodes.item(i);
+                String name = fileTypeNode.getAttributes().getNamedItem("name").getNodeValue();
+                Node extNode = ((Element)fileTypeNode).getElementsByTagName("extensions").item(0);
+                String extensions = extNode.getChildNodes().item(0).getNodeValue();
+                Set<String> extSet = new HashSet<String>(Arrays.asList(extensions.split("\\s+")));
+                fileTypeMap.put(name, extSet);
+            }
+            Set<String> allText = (Set<String>) fileTypeMap.get("text");
+            allText.addAll((Set<String>) fileTypeMap.get("code"));
+            allText.addAll((Set<String>) fileTypeMap.get("xml"));
+            fileTypeMap.put("text", allText);
+            Set<String> allSearchable = (Set<String>) fileTypeMap.get("binary");
+            allSearchable.addAll((Set<String>) fileTypeMap.get("compressed"));
+            allSearchable.addAll((Set<String>) fileTypeMap.get("text"));
+            fileTypeMap.put("searchable", allSearchable);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SAXException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        return fileTypeMap;
+    }
+
+    public FileUtil() {
+        fileTypeMap = getFileTypeMap();
+    }
 
 	public String getExtension(File f) {
 		String ext = "";
@@ -77,18 +83,23 @@ public class FileUtil {
 	}
 
 	public boolean isBinaryFile(File f) {
-		return this._binaryExtensions.contains(this.getExtension(f));
+        return fileTypeMap.get("binary").contains(getExtension(f));
 	}
 
 	public boolean isCompressedFile(File f) {
-		return this._compressedExtensions.contains(this.getExtension(f));
-	}
+        return fileTypeMap.get("compressed").contains(getExtension(f));
+    }
+
+	public boolean isSearchableFile(File f) {
+        return fileTypeMap.get("searchable").contains(getExtension(f));
+    }
 
 	public boolean isTextFile(File f) {
-		return this._textExtensions.contains(this.getExtension(f));
-	}
+        return fileTypeMap.get("text").contains(getExtension(f));
+    }
 
 	public boolean isUnknownFile(File f) {
-		return this._unknownExtensions.contains(this.getExtension(f));
-	}
+        return fileTypeMap.get("unknown").contains(getExtension(f)) ||
+               !fileTypeMap.get("searchable").contains(getExtension(f));
+    }
 }
