7f6f3ebd546ee2bfe92a464f24ff49ca14eb692e
Cary Clark <clarkcb@gmail.com>
2013-05-27 16:52:46 -0700

    Moved search option text into shared xml file

--- python/searchoptions.py
+++ python/searchoptions.py
@@ -10,142 +10,139 @@ from collections import deque
 from cStringIO import StringIO
 import os
 import sys
+import xml.dom.minidom as minidom
 
 from searchoption import SearchOption
 from searchsettings import SearchSettings
 
 class SearchOptions:
-    arg_options = (
-        SearchOption('b', 'linesbefore',
-            'Number of lines to show before every matched line (default: 0)',
+    """class to provide usage info and parse command-line arguments into settings"""
+
+    # TODO: move to a config file
+    SEARCHOPTIONSPATH = '/Users/cary/src/git/xsearch/shared/searchoptions.xml'
+
+    arg_action_dict = {
+        'ext':
             lambda x, settings:
-                settings.set_property('numlinesbefore', int(x))),
-        SearchOption('B', 'linesafter',
-            'Number of lines to show after every matched line (default: 0)',
+                settings.in_extensions.add(x),
+        'extfilter':
             lambda x, settings:
-                settings.set_property('numlinesafter', int(x))),
-        SearchOption('d', 'in-dirpattern',
-            'Specify name pattern for directories to include in search',
+                settings.out_extensions.add(x),
+        'linesbefore':
             lambda x, settings:
-                settings.add_pattern(x, 'in_dirpatterns')),
-        SearchOption('D', 'out-dirpattern',
-            'Specify name pattern for directories to exclude from search',
+                settings.set_property('numlinesbefore', int(x)),
+        'linesafter':
             lambda x, settings:
-                settings.add_pattern(x, 'out_dirpatterns')),
-        SearchOption('f', 'in-filepattern',
-            'Specify name pattern for files to include in search',
+                settings.set_property('numlinesafter', int(x)),
+        'in-dirpattern':
             lambda x, settings:
-                settings.add_pattern(x, 'in_filepatterns')),
-        SearchOption('F', 'out-filepattern',
-            'Specify name pattern for files to exclude from search',
+                settings.add_pattern(x, 'in_dirpatterns'),
+        'out-dirpattern':
             lambda x, settings:
-                settings.add_pattern(x, 'out_filepatterns')),
-        SearchOption('', 'out-linesafterpattern',
-            'Specify pattern to filter the "lines-after" lines on (used with' +
-            ' --linesafter)',
+                settings.add_pattern(x, 'out_dirpatterns'),
+        'in-filepattern':
             lambda x, settings:
-                settings.add_pattern(x, 'out_linesafterpatterns')),
-        SearchOption('', 'in-linesafterpattern',
-            'Specify pattern to search the "lines-after" lines on (used with' +
-            ' --linesafter)',
+                settings.add_pattern(x, 'in_filepatterns'),
+        'out-filepattern':
             lambda x, settings:
-                settings.add_pattern(x, 'in_linesafterpatterns')),
-        SearchOption('', 'out-linesbeforepattern',
-            'Specify pattern to filter the "lines-before" lines on (used with' +
-            ' --linesbefore)',
+                settings.add_pattern(x, 'out_filepatterns'),
+        'out-linesafterpattern':
             lambda x, settings:
-                settings.add_pattern(x, 'out_linesbeforepatterns')),
-        SearchOption('', 'in-linesbeforepattern',
-            'Specify pattern to search the "lines-before" lines on (used with' +
-            ' --linesbefore)',
+                settings.add_pattern(x, 'out_linesafterpatterns'),
+        'in-linesafterpattern':
             lambda x, settings:
-                settings.add_pattern(x, 'in_linesbeforepatterns')),
-        SearchOption('s', 'search',
-            'Specify search pattern',
+                settings.add_pattern(x, 'in_linesafterpatterns'),
+        'out-linesbeforepattern':
             lambda x, settings:
-                settings.add_pattern(x, 'searchpatterns')),
-        SearchOption('x', 'ext',
-            'Specify extension for files to include in search',
+                settings.add_pattern(x, 'out_linesbeforepatterns'),
+        'in-linesbeforepattern':
             lambda x, settings:
-                settings.in_extensions.add(x)),
-        SearchOption('X', 'extfilter',
-            'Specify extension for files to exclude from search',
+                settings.add_pattern(x, 'in_linesbeforepatterns'),
+        'search':
             lambda x, settings:
-                settings.out_extensions.add(x))
-    )
-    flag_options = (
-        SearchOption('1', 'firstmatch',
-            'Capture only the first match for a file+search combination',
+                settings.add_pattern(x, 'searchpatterns')
+    }
+    flag_action_dict = {
+        'allmatches':
             lambda settings:
-                settings.set_property('firstmatch', True)),
-        SearchOption('a', 'allmatches',
-            'Capture all matches*',
+                settings.set_property('firstmatch', False),
+        'firstmatch':
             lambda settings:
-                settings.set_property('firstmatch', False)),
-        SearchOption('', 'debug',
-            'Set output mode to debug',
+                settings.set_property('firstmatch', True),
+        'debug':
             lambda settings:
-                settings.set_property('debug', True)),
-        SearchOption('h', 'help',
-            'Print this usage and exit',
+                settings.set_property('debug', True),
+        'help':
             lambda settings:
-                settings.set_property('printusage', True)),
-        SearchOption('', 'listfiles',
-            'Generate a list of the matching files after searching',
+                settings.set_property('printusage', True),
+        'listfiles':
             lambda settings:
-                settings.set_property('listfiles', True)),
-        SearchOption('', 'listlines',
-            'Generate a list of the matching lines after searching',
+                settings.set_property('listfiles', True),
+        'listlines':
             lambda settings:
-                settings.set_property('listlines', True)),
-        SearchOption('m', 'multilinesearch',
-            'Search files as single multi-line content block',
+                settings.set_property('listlines', True),
+        'multilinesearch':
             lambda settings:
-                settings.set_property('multilinesearch', True)),
-        SearchOption('p', 'printmatches',
-            'Print matches to stdout as found*',
+                settings.set_property('multilinesearch', True),
+        'printmatches':
             lambda settings:
-                settings.set_property('printresults', True)),
-        SearchOption('P', 'noprintmatches',
-            'Suppress printing of matches to stdout',
+                settings.set_property('printresults', True),
+        'noprintmatches':
             lambda settings:
-                settings.set_property('printresults', False)),
-        SearchOption('t', 'dotiming',
-            'Time search execution',
+                settings.set_property('printresults', False),
+        'dotiming':
             lambda settings:
-                settings.set_property('dotiming', True)),
-        SearchOption('v', 'verbose',
-            'Set output mode to verbose',
+                settings.set_property('dotiming', True),
+        'verbose':
             lambda settings:
-                settings.set_property('verbose', True)),
-        SearchOption('V', 'version',
-            'Print version and exit',
+                settings.set_property('verbose', True),
+        'version':
             lambda settings:
-                settings.set_property('printversion', True)),
-        SearchOption('z', 'searchcompressed',
-            'Search compressed files (bz2, gz, tar, zip)*',
+                settings.set_property('printversion', True),
+        'searchcompressed':
             lambda settings:
-                settings.set_property('searchcompressed', True)),
-        SearchOption('Z', 'nosearchcompressed',
-            'Do not search compressed files (bz2, gz, tar, zip)',
+                settings.set_property('searchcompressed', True),
+        'nosearchcompressed':
             lambda settings:
-                settings.set_property('searchcompressed', False))
-    )
+                settings.set_property('searchcompressed', False)
+    }
 
     def __init__(self):
-        self.options = self.arg_options + self.flag_options
+        self.options = []
+        self.arg_dict = {}
+        self.flag_dict = {}
+        self.set_options_from_xml()
         self.sorted_options = sorted(self.options, key=lambda opt: opt.sortarg)
-        self.arg_dict = self.dict_from_options(self.arg_options)
-        self.flag_dict = self.dict_from_options(self.flag_options)
 
-    def dict_from_options(self, options):
-        """Returns a dict for a given collection of SearchOption objects"""
-        opt_dict = {}
-        for o in options:
-            if o.shortarg:
-                opt_dict[o.shortarg] = o
-            opt_dict[o.longarg] = o
-        return opt_dict
+    def get_text(self, nodelist):
+        rc = []
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                rc.append(node.data)
+        return ''.join(rc)
+
+    def set_options_from_xml(self):
+        searchoptionsdom = minidom.parse(self.SEARCHOPTIONSPATH)
+        searchoptionnodes = searchoptionsdom.getElementsByTagName('searchoption')
+        for searchoptionnode in searchoptionnodes:
+            name = searchoptionnode.getAttribute('long')
+            short = searchoptionnode.getAttribute('short')
+            desc = self.get_text(searchoptionnode.childNodes)
+            func = None
+            if name in self.arg_action_dict:
+                func = self.arg_action_dict[name]
+            elif name in self.flag_action_dict:
+                func = self.flag_action_dict[name]
+            option = SearchOption(short, name, desc, func)
+            self.options.append(option)
+            if name in self.arg_action_dict:
+                self.arg_dict[name] = option
+                if short:
+                    self.arg_dict[short] = option
+            elif name in self.flag_action_dict:
+                self.flag_dict[name] = option
+                if short:
+                    self.flag_dict[short] = option
 
     def search_settings_from_args(self, args):
         """Returns a SearchSettings instance for a given list of args"""
