a55b965d757b5e691af7adccbaf7b5e4937e554a
Cary Clark <clarkcb@gmail.com>
2014-04-22 20:30:59 -0700

    More tests for go version

---
+++ go/src/github.com/clarkcb/xsearch/searchresults_test.go
@@ -0,0 +1,51 @@
+package xsearch
+
+import "regexp"
+import "testing"
+
+func TestAddSearchResult(t *testing.T) {
+	searchResults := NewSearchResults()
+
+	searchResult := &SearchResult{
+					regexp.MustCompile("^func\\s+TestAddSearchResult"),
+					"/Users/cary/src/git/xsearch/go/src/github.com/clarkcb/gosearch/searchresults_test.go",
+					6,
+					"func TestAddSearchResult(t *testing.T) {",
+					[]string{},
+					[]string{},
+				}
+	searchResults.AddSearchResult(searchResult)
+
+	if !searchResults.HasSearchResults() {
+		t.Errorf("searchResults.HasSearchResults()=%t, expected=true",
+			searchResults.HasSearchResults())
+	}
+
+	if len(searchResults.SearchResults) != 1 {
+		t.Errorf("len(searchResults.SearchResults)=%d, expected=1",
+			len(searchResults.SearchResults))
+	}
+
+	expectedDir := "/Users/cary/src/git/xsearch/go/src/github.com/clarkcb/gosearch"
+	c, b := searchResults.DirCounts[expectedDir]
+	if !b {
+		t.Errorf("searchResults.DirCounts does not contain expected dir key: %s",
+			expectedDir)
+	}
+	if c != 1 {
+		t.Errorf("searchResults.DirCounts[\"%s\"]=%d, expected=1",
+			expectedDir, searchResults.DirCounts[expectedDir])
+	}
+
+	expectedFile := "/Users/cary/src/git/xsearch/go/src/github.com/clarkcb/gosearch/searchresults_test.go"
+	c, b = searchResults.FileCounts[expectedFile]
+	if !b {
+		t.Errorf("searchResults.FileCounts does not contain expected file key: %s",
+			expectedFile)
+	}
+	if c != 1 {
+		t.Errorf("searchResults.FileCounts[\"%s\"]=%d, expected=1",
+			expectedFile, searchResults.FileCounts[expectedFile])
+	}
+
+}
---
+++ go/src/github.com/clarkcb/xsearch/searchutils_test.go
@@ -0,0 +1,61 @@
+package xsearch
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	greetings := []string{
+		"hi", "hello", "g'day", "ahoy", "greetings", "hey",
+	}
+
+	others := []string{
+		"привет", "здравствуйте", "こんにちは", 
+	}
+
+	b := false
+	for _, g := range greetings {
+		if b = contains(greetings, g); !b {
+			t.Errorf("contains(greetings, \"%s\")=%t, expected=true", b)
+		}
+	}
+
+	for _, g := range others {
+		if b = contains(greetings, g); b {
+			t.Errorf("contains(greetings, \"%s\")=%t, expected=false", b)
+		}
+	}
+}
+
+func TestGetLongestLen(t *testing.T) {
+	greetings := []string{
+		"hi", "hello", "g'day", "ahoy", "greetings", "hey",
+	}
+
+	longestLen := getLongestLen(greetings)
+	expectedLongestLen := 9
+
+	if longestLen != expectedLongestLen {
+		t.Errorf("longestLen: %d, expected=%d", longestLen, expectedLongestLen)
+	}
+}
+
+func TestGetSortedKeys(t *testing.T) {
+	greetings := []string{
+		"hi", "hello", "g'day", "ahoy", "greetings", "hey",
+	}
+	greetingsMap := map[string]string{}
+	for _, g := range greetings {
+		greetingsMap[g] = g
+	}
+
+	sortedGreetings := getSortedKeys(greetingsMap)
+
+	if sortedGreetings[0] != "ahoy" {
+		t.Errorf("sortedGreetings[0]=\"%s\", expected=\"ahoy\"",
+			sortedGreetings[0])
+	}
+
+	if sortedGreetings[len(sortedGreetings)-1] != "hi" {
+		t.Errorf("sortedGreetings[%d]=\"%s\", expected=\"hi\"",
+			len(sortedGreetings)-1, sortedGreetings[len(sortedGreetings)-1])
+	}
+}
