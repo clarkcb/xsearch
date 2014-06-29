7f413b3f1547108901680a45fa62d50b51fc4a09
Cary Clark <clarkcb@gmail.com>
2014-06-28 23:31:23 -0700

    Added universal build script

---
+++ shared/build.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+################################################################################
+#
+# build.sh
+#
+# Builds specified language version of xsearch, or all versions
+#
+################################################################################
+
+########################################
+# Configuration
+########################################
+
+PROJECT_PATH=/Users/cary/src/git/xsearch
+SHARED_PATH=$PROJECT_PATH/shared
+
+
+########################################
+# Utility Functions
+########################################
+
+# log(msg)
+log () {
+    local msg="$1" dt=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$dt] $msg"
+}
+
+# copy_resources
+copy_resources () {
+    local resources_path="$1"
+    log "cp $SHARED_PATH/filetypes.xml $resources_path/"
+    cp $SHARED_PATH/filetypes.xml $resources_path/
+    log "cp $SHARED_PATH/searchoptions.xml $resources_path/"
+    cp $SHARED_PATH/searchoptions.xml $resources_path/
+}
+
+########################################
+# Build Functions
+########################################
+
+build_csharp () {
+    log "build_csharp"
+    CSHARP_PATH=$PROJECT_PATH/csharp
+    RESOURCES_PATH=$CSHARP_PATH/CsSearch/CsSearch/Resources
+
+    # copy the shared xml files to the local resource location
+    copy_resources $RESOURCES_PATH
+
+    # run a mono xbuild
+    log "Building cssearch"
+    xbuild /p:Configuration=Debug $CSHARP_PATH/CsSearch/CsSearch.sln
+
+}
+
+build_fsharp () {
+    log "build_fsharp"
+    FSHARP_PATH=$PROJECT_PATH/fsharp
+    RESOURCES_PATH=$FSHARP_PATH/FsSearch/Resources
+
+    # copy the shared xml files to the local resource location
+    copy_resources $RESOURCES_PATH
+
+    # run a mono xbuild
+    log "Building fssearch"
+    xbuild /p:Configuration=Debug $FSHARP_PATH/FsSearch.sln
+}
+
+build_go () {
+    log "build_go"
+    export GOPATH=$PROJECT_PATH/go
+    export PATH=$GOPATH/bin:$PATH
+
+    # build the code to generate the dynamic code for gosearch
+    log "Building gengosearchcode"
+    go install elocale.com/clarkcb/gosearchcodegen/gengosearchcode
+
+    # run it to generate the dynamic gosearch code
+    log "Running gengosearchcode"
+    gengosearchcode
+
+    # now build gosearch
+    log "Building gosearch"
+    go install elocale.com/clarkcb/xsearch/gosearch
+}
+
+build_java () {
+    log "build_java"
+    JAVA_PATH=$PROJECT_PATH/java
+    RESOURCES_PATH=$JAVA_PATH/src/main/resources
+
+    # copy the shared xml files to the local resource location
+    copy_resources $RESOURCES_PATH
+
+    # run a maven clean build
+    log "Building javasearch"
+    mvn -f $JAVA_PATH/pom.xml clean install
+}
+
+build_scala () {
+    log "build_scala"
+    SCALA_PATH=$PROJECT_PATH/scala
+    RESOURCES_PATH=$SCALA_PATH/src/main/resources
+
+    # copy the shared xml files to the local resource location
+    copy_resources $RESOURCES_PATH
+
+    # run a maven clean build
+    log "Building scalasearch"
+    mvn -f $SCALA_PATH/pom.xml clean install
+}
+
+build_all () {
+    log "build_all"
+    
+    build_csharp
+
+    build_fsharp
+
+    build_go
+
+    build_java
+
+    build_scala
+}
+
+
+########################################
+# Build Steps
+########################################
+
+if [ $# == 0 ]; then
+    ARG="all"
+else
+    ARG=$1
+fi
+
+if [ "$ARG" == "all" ]; then
+    build_all
+elif [ "$ARG" == "csharp" ]; then
+    build_csharp
+elif [ "$ARG" == "fsharp" ]; then
+    build_fsharp
+elif [ "$ARG" == "go" ]; then
+    build_go
+elif [ "$ARG" == "java" ]; then
+    build_java
+elif [ "$ARG" == "scala" ]; then
+    build_scala
+else
+    echo "ERROR: unknown build argument: $ARG"
+fi
+
+
