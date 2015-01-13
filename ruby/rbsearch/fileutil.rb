6aebcb72857e2a8956eed0f5e2dcae55ede52af0
Cary Clark <clarkcb@gmail.com>
2015-01-13 15:24:29 -0800

    New version of FileUtil for ruby

---
+++ ruby/rbsearch/fileutil.rb
@@ -0,0 +1,19 @@
+################################################################################
+#
+# fileutil.rb
+#
+# class FileUtil: provides utility functions for getting file extension and
+#                 determining file type
+#
+################################################################################
+
+class FileUtil
+  def self.get_extension(filename)
+    ext = ''
+    index = filename.rindex('.')
+    if index and index < (filename.length - 1)
+      ext = filename[index+1..filename.length].downcase
+    end
+    ext
+  end
+end
