192aad9cc411b318f6e764810792e50cc73d99c4
Cary Clark <clarkcb@gmail.com>
2015-01-13 18:11:39 -0800

    New FileUtil static class in C#

---
+++ csharp/CsSearch/CsSearch/FileUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CsSearch
+{
+	class FileUtil
+	{
+		public static string GetRelativePath(string fullPath)
+		{
+			var filePath = fullPath;
+			if (filePath.StartsWith(Environment.CurrentDirectory))
+				filePath = filePath.Replace(Environment.CurrentDirectory, ".");
+			return filePath;
+		}
+
+		public static bool IsHiddenFile(FileSystemInfo f)
+		{
+			return (f.Attributes & FileAttributes.Hidden) != 0;
+		}
+	}
+}
