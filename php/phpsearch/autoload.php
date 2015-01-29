a5e64669026239b4c811dd492df00ccfa3ef3304
Cary Clark <clarkcb@gmail.com>
2015-01-29 15:47:58 -0800

    Use HOME env var in php

--- php/phpsearch/autoload.php
+++ php/phpsearch/autoload.php
@@ -3,7 +3,8 @@
 // the str_replace was necessary when using a namespace because it was using
 // a backslash in the path instead of forward slash
 spl_autoload_register(function ($class) {
-    $include_path = '/Users/cary/src/git/xsearch/php/phpsearch/';
+	$HOME = getenv('HOME');
+    $include_path = "$HOME/src/git/xsearch/php/phpsearch/";
     include $include_path . str_replace("\\", "/", strtolower($class)) . '.php';
 });
 
