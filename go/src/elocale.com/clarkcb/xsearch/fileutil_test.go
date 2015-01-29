069ece74808af0275a85c5d02a72fa41440235e0
Cary Clark <clarkcb@gmail.com>
2015-01-29 00:38:57 -0800

    Added fileutil unit tests for go

--- go/src/elocale.com/clarkcb/xsearch/fileutil_test.go
+++ go/src/elocale.com/clarkcb/xsearch/fileutil_test.go
@@ -16,3 +16,54 @@ func TestGetExtension(t *testing.T) {
 		}
 	}
 }
+
+func TestIsDotDir(t *testing.T) {
+	expected := map[string]bool{
+		".":      true,
+		"..":     true,
+		"lib.a":  false,
+		"noext":  false,
+	}
+
+	for k, v := range expected {
+		if d := isDotDir(k); d != v {
+			t.Errorf("isDotDir(\"%s\")=%v, expected=%v", k, d, v)
+		}
+	}
+}
+
+func TestIsHidden(t *testing.T) {
+	expected := map[string]bool{
+		".":          false,
+		"..":         false,
+		"lib.a":      false,
+		"noext":      false,
+		".git":       true,
+		".gitignore": true,
+	}
+
+	for k, v := range expected {
+		if h := isHidden(k); h != v {
+			t.Errorf("isHidden(\"%s\")=%v, expected=%v", k, h, v)
+		}
+	}
+}
+
+func TestNormalizePath(t *testing.T) {
+	expected := map[string]string{
+		".":          ".",
+		"./":         ".",
+		"..":         "..",
+		"../":        "..",
+		"path":       "path",
+		"path/":      "path",
+		"long/path":  "long/path",
+		"long/path/": "long/path",
+	}
+
+	for k, v := range expected {
+		if p := normalizePath(k); p != v {
+			t.Errorf("normalizePath(\"%s\")=\"%s\", expected=\"%s\"", k, p, v)
+		}
+	}
+}
