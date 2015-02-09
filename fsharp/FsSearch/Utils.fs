978e59372844a649c8d5544755d18eb1d689afa9
Cary Clark <clarkcb@gmail.com>
2015-02-09 14:43:12 -0800

    New version of Utils in F#

---
+++ fsharp/FsSearch/Utils.fs
@@ -0,0 +1,21 @@
+﻿namespace FsSearch
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Text
+open System.Xml.Linq
+
+module Utils = 
+    let PrintElapsed (name : string) (ts : TimeSpan) =
+        let elapsedTime =
+            String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10)
+        printfn "Elapsed time for %s: %s" name elapsedTime
+
+    let PrintNames (names : string list) =
+        for name in names do
+            printfn "Name: %s" name
+
+;;
