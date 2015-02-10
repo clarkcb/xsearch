2161203f90a427e48f1ffb397e063cf55369f54f
Cary Clark <clarkcb@gmail.com>
2015-02-10 00:01:58 -0800

    New version of FileUtil in F#

---
+++ fsharp/FsSearch/FileUtil.fs
@@ -0,0 +1,44 @@
+﻿namespace FsSearch
+
+open System
+open System.IO
+
+module FileUtil = 
+
+    let dotDirs = Set.ofList ["."; ".."]
+
+    let GetHomePath () : string = 
+        match Environment.GetEnvironmentVariable("HOME") with
+        | home when home <> null -> home
+        | _ -> Environment.GetEnvironmentVariable("USERPROFILE")
+
+    let GetFileContents (filepath : string) =
+        let contents =
+            try
+                use sr = new StreamReader (filepath)
+                sr.ReadToEnd()
+            with
+            | :? IOException as e -> printfn "%s" e.Message; ""
+        contents
+
+    let ExpandPath (filepath : string) =
+        if filepath.[0] = '~' then GetHomePath() + filepath.Substring(1)
+        else filepath
+
+    let IsDirectory (filepath : string) =
+        let isDir =
+            try
+                let attr = File.GetAttributes(filepath)
+                attr &&& FileAttributes.Directory = FileAttributes.Directory
+            with
+            | :? DirectoryNotFoundException -> false
+            | :? FileNotFoundException -> false
+        isDir
+
+    let IsDotDir (filepath : string) = dotDirs.Contains(filepath)
+
+    let IsHidden (f : FileSystemInfo) : bool = 
+        let startsWithDot = f.Name.[0] = '.' && not (IsDotDir f.Name)
+        //let hasHiddenAttribute = f.Exists && (f.Attributes &&& FileAttributes.Hidden) <> 0
+        startsWithDot
+    ;;
