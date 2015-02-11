d9b884c80c98650a4167a60171f0c7ab2476dcd5
Cary Clark <clarkcb@gmail.com>
2015-02-11 01:05:14 -0800

    Added GetFileLines to FileUtil in F#

--- fsharp/FsSearch/FileUtil.fs
+++ fsharp/FsSearch/FileUtil.fs
@@ -25,20 +25,26 @@ module FileUtil =
         if filepath.[0] = '~' then GetHomePath() + filepath.Substring(1)
         else filepath
 
+    let IsDotDir (filepath : string) = dotDirs.Contains(filepath)
+
     let IsDirectory (filepath : string) =
-        let isDir =
+        if IsDotDir filepath then true
+        else
             try
                 let attr = File.GetAttributes(filepath)
                 attr &&& FileAttributes.Directory = FileAttributes.Directory
             with
             | :? DirectoryNotFoundException -> false
             | :? FileNotFoundException -> false
-        isDir
-
-    let IsDotDir (filepath : string) = dotDirs.Contains(filepath)
 
     let IsHidden (filepath : string) : bool = 
         let startsWithDot = filepath.[0] = '.' && not (IsDotDir filepath)
         //let hasHiddenAttribute = f.Exists && (f.Attributes &&& FileAttributes.Hidden) <> 0
         startsWithDot
+
+    let GetFileLines (filePath : string) = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
     ;;
