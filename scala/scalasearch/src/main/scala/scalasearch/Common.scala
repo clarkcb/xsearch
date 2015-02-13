036a48ca7580a0b42ecfc62e65890ecc89532890
Cary Clark <clarkcb@gmail.com>
2015-02-12 18:21:04 -0800

    Moved log to new Common object in scala

---
+++ scala/scalasearch/src/main/scala/scalasearch/Common.scala
@@ -0,0 +1,11 @@
+package scalasearch
+
+object Common {
+  def log(message: String): Unit = {
+    println(message)
+  }
+
+  def logError(message: String): Unit = {
+    log("Error: " + message)
+  }
+}
