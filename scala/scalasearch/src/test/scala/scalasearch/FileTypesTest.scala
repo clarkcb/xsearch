c541c076bbf8bb0077bfc29de474269f45b793a3
Cary Clark <clarkcb@gmail.com>
2015-03-27 16:26:07 -0700

    Updated FileTypesTest in scala

--- scala/scalasearch/src/test/scala/scalasearch/FileTypesTest.scala
+++ scala/scalasearch/src/test/scala/scalasearch/FileTypesTest.scala
@@ -9,9 +9,8 @@ import org.scalatest.junit.JUnitRunner
 class FileTypesTest extends FunSuite {
 
   test("test archive extensions") {
-    """bz2 cpio ear gz hqx jar pax rar sit sitx tar tgz war zip zipx Z""".
-      split("\\s+").foreach {
-      ext =>
+    """7z arj bz2 cpio ear gz hqx jar pax rar sit sitx tar tgz war zip zipx Z""".
+      split("\\s+").foreach { ext =>
         val archiveFile = new File("archive."+ext)
         println("archilveFile: "+archiveFile)
         assert(!FileTypes.isBinaryFile(archiveFile))
@@ -25,10 +24,10 @@ class FileTypesTest extends FunSuite {
   }
 
   test("test binary extensions") {
-    """a ai beam bin chm class com dat dbmdl dcr dir dll dxr dms doc dot exe hi
-      |hlp indd lnk mdb mo nib o obj odt pdb ppt psd pyc pyo qxd so swf sys vsd
-      |xls xlt""".stripMargin.split("\\s+").foreach {
-      ext =>
+    """a ai beam bin chm class com dat dbmdl dcr dir dll dms doc dot dxr dylib
+      |epub exe fm hi hlp indd lib lnk mdb mo mobi mpp nib o obj odm odt ott
+      |pages pdb ppt psd pub pyc pyo qxd rpt so swf sys vsd wpd wps wpt wri
+      |xls xlt""".stripMargin.split("\\s+").foreach { ext =>
         val binFile = new File("binfile."+ext)
         println("binFile: "+binFile)
         assert(FileTypes.isBinaryFile(binFile))
@@ -42,11 +41,12 @@ class FileTypesTest extends FunSuite {
   }
 
   test("test code extensions") {
-    """applejs as bash bat boo bsh c c++ cc cgi clj coffee cpp cs csh cxx el
-      |erl es fs fx go groovy h h++ hh hpp hs java js js2 jsf json jsp jspf m
-      |pas php php3 php4 php5 pl pm pxd pxi py pyw pyx rb rs sbt sc scala sh
-      |tcl vb""".stripMargin.split("\\s+").foreach {
-      ext =>
+    """ada adb ads applejs as asm au3 bas bash bat boo bsh c c++ cbl cc cfm cgi
+      |clj cls cmd cob coffee cpp cs csh cxx d e el elm erl es ex exs frm fs fth
+      |fx go groovy h h++ hh hpp hs java js js2 jsf json jsp jspf kt lhs lisp
+      |lua m ml pas php php3 php4 php5 pl pm ps1 psc1 psd1 psm1 pxd pxi py pyw
+      |pyx r rb rkt rs s sass sbt sc scm scss scala sh swift tcl ts vb vbs""".
+      stripMargin.split("\\s+").foreach { ext =>
         val codeFile = new File("codefile."+ext)
         println("codeFile: "+codeFile)
         assert(!FileTypes.isBinaryFile(codeFile))
@@ -60,11 +60,11 @@ class FileTypesTest extends FunSuite {
   }
 
   test("test nosearch extensions") {
-    """aif aifc aiff au avi bmp cab db dmg eps gif icns ico idlk ief iso jpe
-      |jpeg jpg m3u m4a m4p mov movie mp3 mp4 mpe mpeg mpg mxu ogg pdf pict
-      |png ps qt ra ram rm rpm snd suo tif tiff wav""".
-      stripMargin.split("\\s+").foreach {
-      ext =>
+    """aif aifc aiff au avi bmp cab cur db dib dmg eot gif icns ico
+      |idlk ief iso jpe jpeg jpg m3u m4a m4p mov movie mp3 mp4 mpe mpeg mpg mxu
+      |ogg otf pdf pict png qt ra ram rm rpm snd suo tif tiff tte ttf wav
+      |woff""".
+      stripMargin.split("\\s+").foreach { ext =>
         val nosearchFile = new File("nosearch."+ext)
         println("nosearchFile: "+nosearchFile)
         assert(!FileTypes.isBinaryFile(nosearchFile))
@@ -78,17 +78,17 @@ class FileTypesTest extends FunSuite {
   }
 
   test("test text extensions") {
-    """1 10 11 12 13 14 15 16 17 18 19 2 20 3 323 4 5 6 7 8 9 am app asc ascx
-      |asm asp aspx bib brf cfg cls cmd cnt conf css csv ctl d dbml dbschema
-      |ddl dep dfm diff disco dlg dof dpr drl dsp dsw dtd elt ent env etx exp
-      |feature fls gcd hql hs htc htm html hxx ics icz iml in inc ini ipr iws
-      |jad jam jql layout lhs log ltx mak mako manifest map master md mf mht
-      |mml moc mod mxml p patch plist pm po pot properties pt rc rc2 rdf rex
-      |rtf rtx scc sct sfv sgm sgml sht shtm shtml sln smi smil spec sqc sql st
-      |str strings sty suml sxw t tex text tk tld tm tmx tsv txt ui uls uml url
-      |user vbs vcf vcs vm vrml vssscc vxml wbxml webinfo wml wmls wrl wsc wsd
-      |wsdd xlf yaml yml""".stripMargin.split("\\s+").foreach {
-      ext =>
+    """1 10 11 12 13 14 15 16 17 18 19 2 20 3 323 4 5 6 7 8 9 am app
+      |asc ascx asm asmx asp aspx bib brf cabal cfg cls cmake cmd cnt conf css
+      |csv ctl d dbml dbschema ddl dep dfm diff disco dlg dof dpr drl dsp dsw
+      |dtd elt ent env eps etx exp feature fls gcd hql hs htc htm html hxx ics
+      |icz iml in inc ini ipr iws jad jam jql layout lhs log ltx mak mako
+      |manifest map markdown master md mf mht mml moc mod mxml p patch plist pm
+      |po pot properties ps pt rc rc2 rdf rex rtf rtx scc sct sfv sgm sgml sht
+      |shtm shtml sln smi smil spec sqc sql st str strings sty suml sxw t tex
+      |text tk tld tm tmx tsv txt ui uls uml url user vbs vcf vcs vm vrml vssscc
+      |vxml wbxml webinfo wml wmls wrl wsc wsd wsdd xlf xsp yaml
+      |yml""".stripMargin.split("\\s+").foreach { ext =>
         val textFile = new File("textFile."+ext)
         println("textFile: "+textFile)
         assert(!FileTypes.isBinaryFile(textFile))
@@ -103,8 +103,7 @@ class FileTypesTest extends FunSuite {
 
   test("test unknown extensions") {
     """adm aps cli clw def df2 ncb nt nt2 orig pc plg roff sun texinfo tr xwd""".
-      split("\\s+").foreach {
-      ext =>
+      split("\\s+").foreach { ext =>
         val unknownFile = new File("unknown."+ext)
         println("unknownFile: "+unknownFile)
         assert(!FileTypes.isBinaryFile(unknownFile))
@@ -119,11 +118,10 @@ class FileTypesTest extends FunSuite {
 
   test("test xml extensions") {
     """atom atomcat atomsrv bdsproj config csproj davmount dbproj docx dotx
-      |fsproj fxml jhm jnlp kml pom potx ppsx pptx qrc rdf resx rng rss
-      |settings sldx stc std sti stw svg svgz sxc sxd sxg sxi sxm sxw tld
-      |vbproj vcproj vdproj wadl wsdd wsdl x3d xaml xhtml xht xjb xlsx xltx
-      |xml xsd xsl xslt xspf xul""".stripMargin.split("\\s+").foreach {
-      ext =>
+      |fsproj fxml jhm jnlp kml mm pom potx ppsx pptx qrc rdf resx rng rss
+      |settings sldx stc std sti stw svg svgz sxc sxd sxg sxi stw sxm sxw tld
+      |vbproj vcproj vdproj wadl wsdd wsdl x3d xaml xhtml xht xjb xlsx xltx xml
+      |xps xsd xsl xslt xspf xul""".stripMargin.split("\\s+").foreach { ext =>
         val xmlFile = new File("xmlfile."+ext)
         println("xmlFile: "+xmlFile)
         assert(!FileTypes.isBinaryFile(xmlFile))
