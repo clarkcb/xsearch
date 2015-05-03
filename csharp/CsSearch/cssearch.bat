36ad4eef3414171d62f0f21c91358ec558318f85
Cary Clark <clarkcb@gmail.com>
2015-05-02 18:12:20 -0700

    Added bat files for C# and F#

---
+++ csharp/CsSearch/cssearch.bat
@@ -0,0 +1,2 @@
+@echo off
+%USERPROFILE%\src\git\xsearch\csharp\CsSearch\CsSearch\bin\Debug\CsSearch.exe %*
---
+++ fsharp/fssearch.bat
@@ -0,0 +1,2 @@
+@echo off
+%USERPROFILE%\src\git\xsearch\fsharp\fssearch\bin\Debug\FsSearch.exe %*
