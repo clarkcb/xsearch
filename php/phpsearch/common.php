b7c72b94c52d8b18e327eb3d78f49fab69c691d9
Cary Clark <clarkcb@gmail.com>
2015-06-09 17:57:31 -0700

    Simplified search result sorting in php

--- php/phpsearch/common.php
+++ php/phpsearch/common.php
@@ -10,17 +10,16 @@ function cmp_ignorecase($s1, $s2) {
 
 function cmp_searchresults($r1, $r2) {
     $dircmp = cmp_ignorecase(dirname($r1->file), dirname($r2->file));
-    if ($dircmp === 0) {
-        $filecmp = cmp_ignorecase(basename($r1->file), basename($r2->file));
-        if ($filecmp === 0) {
-            $linenumcmp = $r1->linenum - $r2->linenum;
-        	if ($linenumcmp === 0) {
-        		return $r1->match_start_index - $r2->match_start_index;
-            }
-        	return $linenumcmp;
-        }
-    	return $filecmp;
+    if ($dircmp !== 0) {
+        return $dircmp;
     }
-    return $dircmp;
+    $filecmp = cmp_ignorecase(basename($r1->file), basename($r2->file));
+    if ($filecmp !== 0) {
+        return $filecmp;
+    }
+	if ($r1->linenum === $r2->linenum) {
+		return $r1->match_start_index - $r2->match_start_index;
+    }
+	return $r1->linenum - $r2->linenum;
 }
 ?>
