dd44af722c1893a1d061567e28970bca55d151f3
Cary Clark <clarkcb@gmail.com>
2015-08-04 15:24:00 -0700

    Minor enhancements to cssearch.sh

--- csharp/CsSearch/cssearch.sh
+++ csharp/CsSearch/cssearch.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-XSEARCH_PATH=$HOME/src/xsearch
-CSSEARCH_PATH=$XSEARCH_PATH/csharp/CsSearch
+
+SCRIPTPATH=$( readlink "${BASH_SOURCE[0]}" )
+SCRIPTDIR=$( dirname "$SCRIPTPATH" )
+source "$SCRIPTDIR/../../scripts/config.sh" 
+
+CSSEARCH_PATH=$CSHARP_PATH/CsSearch
 CONFIGURATION=Debug
 #CONFIGURATION=Release
 CSSEARCHEXE=$CSSEARCH_PATH/CsSearch/bin/$CONFIGURATION/CsSearch.exe
