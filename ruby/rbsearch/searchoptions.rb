e798f5e14d5c3670204d2bf2f2ad5ff734711b11
Cary Clark <clarkcb@gmail.com>
2015-08-04 15:18:46 -0700

    Changed dict keys to symbols in ruby

--- ruby/rbsearch/searchoptions.rb
+++ ruby/rbsearch/searchoptions.rb
@@ -26,69 +26,69 @@ class SearchOptions
 
   def set_actions
     @arg_action_dict = {
-      'in-archiveext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
+      :'in-archiveext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
           settings.in_archiveextensions) },
-      'in-archivefilepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'in-archivefilepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.in_archivefilepatterns) },
-      'in-dirpattern' => ->(x, settings){ settings.add_pattern(x,
+      :'in-dirpattern' => ->(x, settings){ settings.add_pattern(x,
         settings.in_dirpatterns) },
-      'in-ext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
+      :'in-ext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
           settings.in_extensions) },
-      'in-filepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'in-filepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.in_filepatterns) },
-      'in-linesafterpattern' => ->(x, settings){ settings.add_pattern(x,
+      :'in-linesafterpattern' => ->(x, settings){ settings.add_pattern(x,
         settings.in_linesafterpatterns) },
-      'in-linesbeforepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'in-linesbeforepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.in_linesbeforepatterns) },
-      'linesafter' => ->(x, settings){ settings.linesafter = x.to_i },
-      'linesaftertopattern' => ->(x, settings){ settings.add_pattern(x,
+      :'linesafter' => ->(x, settings){ settings.linesafter = x.to_i },
+      :'linesaftertopattern' => ->(x, settings){ settings.add_pattern(x,
         settings.linesaftertopatterns) },
-      'linesafteruntilpattern' => ->(x, settings){ settings.add_pattern(x,
+      :'linesafteruntilpattern' => ->(x, settings){ settings.add_pattern(x,
         settings.linesafteruntilpatterns) },
-      'linesbefore' => ->(x, settings){ settings.linesbefore = x.to_i },
-      'maxlinelength' => ->(x, settings){ settings.maxlinelength = x.to_i },
-      'out-archiveext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
+      :'linesbefore' => ->(x, settings){ settings.linesbefore = x.to_i },
+      :'maxlinelength' => ->(x, settings){ settings.maxlinelength = x.to_i },
+      :'out-archiveext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
           settings.out_archiveextensions) },
-      'out-archivefilepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'out-archivefilepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.out_archivefilepatterns) },
-      'out-dirpattern' => ->(x, settings){ settings.add_pattern(x,
+      :'out-dirpattern' => ->(x, settings){ settings.add_pattern(x,
         settings.out_dirpatterns) },
-      'out-ext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
+      :'out-ext' => ->(x, settings){ settings.add_comma_delimited_exts(x,
           settings.out_extensions) },
-      'out-filepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'out-filepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.out_filepatterns) },
-      'out-linesafterpattern' => ->(x, settings){ settings.add_pattern(x,
+      :'out-linesafterpattern' => ->(x, settings){ settings.add_pattern(x,
         settings.out_linesafterpatterns) },
-      'out-linesbeforepattern' => ->(x, settings){ settings.add_pattern(x,
+      :'out-linesbeforepattern' => ->(x, settings){ settings.add_pattern(x,
         settings.out_linesbeforepatterns) },
-      'search' => ->(x, settings){ settings.add_pattern(x,
+      :'search' => ->(x, settings){ settings.add_pattern(x,
         settings.searchpatterns) }
     }
     @flag_action_dict = {
-      'allmatches' => ->(settings){ settings.firstmatch = false },
-      'archivesonly' => ->(settings){ settings.archivesonly = true;
+      :'allmatches' => ->(settings){ settings.firstmatch = false },
+      :'archivesonly' => ->(settings){ settings.archivesonly = true;
         settings.searcharchives = true },
-      'caseinsensitive' => ->(settings){ settings.casesensitive = false },
-      'casesensitive' => ->(settings){ settings.casesensitive = true },
-      'debug' => ->(settings){ settings.debug = true; settings.verbose = true },
-      'dotiming' => ->(settings){ settings.dotiming = true },
-      'excludehidden' => ->(settings){ settings.excludehidden = true },
-      'firstmatch' => ->(settings){ settings.firstmatch = true },
-      'help' => ->(settings){ settings.printusage = true },
-      'includehidden' => ->(settings){ settings.excludehidden = false },
-      'listdirs' => ->(settings){ settings.listdirs = true },
-      'listfiles' => ->(settings){ settings.listfiles = true },
-      'listlines' => ->(settings){ settings.listlines = true },
-      'multilinesearch' => ->(settings){ settings.multilinesearch = true },
-      'noprintmatches' => ->(settings){ settings.printresults = false },
-      'norecursive' => ->(settings){ settings.recursive = false },
-      'nosearcharchives' => ->(settings){ settings.searcharchives = false },
-      'printmatches' => ->(settings){ settings.printresults = true },
-      'recursive' => ->(settings){ settings.recursive = true },
-      'searcharchives' => ->(settings){ settings.searcharchives = true },
-      'uniquelines' => ->(settings){ settings.uniquelines = true },
-      'verbose' => ->(settings){ settings.verbose = true },
-      'version' => ->(settings){ settings.printversion = true }
+      :'caseinsensitive' => ->(settings){ settings.casesensitive = false },
+      :'casesensitive' => ->(settings){ settings.casesensitive = true },
+      :'debug' => ->(settings){ settings.debug = true; settings.verbose = true },
+      :'dotiming' => ->(settings){ settings.dotiming = true },
+      :'excludehidden' => ->(settings){ settings.excludehidden = true },
+      :'firstmatch' => ->(settings){ settings.firstmatch = true },
+      :'help' => ->(settings){ settings.printusage = true },
+      :'includehidden' => ->(settings){ settings.excludehidden = false },
+      :'listdirs' => ->(settings){ settings.listdirs = true },
+      :'listfiles' => ->(settings){ settings.listfiles = true },
+      :'listlines' => ->(settings){ settings.listlines = true },
+      :'multilinesearch' => ->(settings){ settings.multilinesearch = true },
+      :'noprintmatches' => ->(settings){ settings.printresults = false },
+      :'norecursive' => ->(settings){ settings.recursive = false },
+      :'nosearcharchives' => ->(settings){ settings.searcharchives = false },
+      :'printmatches' => ->(settings){ settings.printresults = true },
+      :'recursive' => ->(settings){ settings.recursive = true },
+      :'searcharchives' => ->(settings){ settings.searcharchives = true },
+      :'uniquelines' => ->(settings){ settings.uniquelines = true },
+      :'verbose' => ->(settings){ settings.verbose = true },
+      :'version' => ->(settings){ settings.printversion = true }
     }
   end
 
@@ -99,21 +99,21 @@ class SearchOptions
       short = searchoption.attributes['short']
       desc = searchoption.text.strip
       func = nil
-      if @arg_action_dict.has_key?(long)
-        func = @arg_action_dict[long]
-      elsif @flag_action_dict.has_key?(long)
-        func = @flag_action_dict[long]
+      if @arg_action_dict.has_key?(long.to_sym)
+        func = @arg_action_dict[long.to_sym]
+      elsif @flag_action_dict.has_key?(long.to_sym)
+        func = @flag_action_dict[long.to_sym]
       else
         raise ArgumentError, "Unknown search option: #{long}"
       end
       option = SearchOption.new(short, long, desc, func)
       @options.push(option)
-      if @arg_action_dict.has_key?(long)
+      if @arg_action_dict.has_key?(long.to_sym)
         @arg_dict[long] = option
         if short
           @arg_dict[short] = option
         end
-      elsif @flag_action_dict.has_key?(long)
+      elsif @flag_action_dict.has_key?(long.to_sym)
         @flag_dict[long] = option
         if short
           @flag_dict[short] = option
