ffeb14ad28b637c1c04b71f7e0a7b08fbcd84ea7
Cary Clark <clarkcb@gmail.com>
2016-01-08 01:02:23 -0800

    Load shared config.json in php

--- php/phpsearch/config.php
+++ php/phpsearch/config.php
@@ -1,9 +1,21 @@
 <?php
 
+$currdir = realpath(dirname(__FILE__));
+$config_rel_path = '../../shared/config.json';
+$config_json_path = $currdir . '/' . $config_rel_path;
+$config = json_decode(file_get_contents($config_json_path));
+
+$sharedpath = $config->{'xsearchpath'} . '/shared';
+
+define('Z_XSEARCHPATH', $config->{'xsearchpath'});
+define('Z_SHAREDPATH', $sharedpath);
+define('Z_FILETYPESPATH', $sharedpath . '/filetypes.xml');
+define('Z_SEARCHOPTIONSPATH', $sharedpath . '/searchoptions.xml');
+
 class Config {
-    const XSEARCHPATH = '~/src/xsearch';
-    const SHAREDPATH = '~/src/xsearch/shared';
-    const FILETYPESPATH = '~/src/xsearch/shared/filetypes.xml';
-    const SEARCHOPTIONSPATH = '~/src/xsearch/shared/searchoptions.xml';
+    const XSEARCHPATH = Z_XSEARCHPATH;
+    const SHAREDPATH = Z_SHAREDPATH;
+    const FILETYPESPATH = Z_FILETYPESPATH;
+    const SEARCHOPTIONSPATH = Z_SEARCHOPTIONSPATH;
 }
 ?>
