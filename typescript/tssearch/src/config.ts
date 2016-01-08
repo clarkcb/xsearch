a0fa9dcc97d43bd7364c7dbca82eede00ecb31c0
Cary Clark <clarkcb@gmail.com>
2016-01-07 20:48:44 -0800

    Load shared config.json in javascript and typescript

--- typescript/tssearch/src/config.ts
+++ typescript/tssearch/src/config.ts
@@ -7,11 +7,14 @@
 
 "use strict";
 
+var config = require('../../../shared/config.json');
+
 var isWin: boolean = /^win/.test(process.platform);
 
 var HOME_NAME: string = isWin ? 'USERPROFILE' : 'HOME';
 var HOME: string = process.env[HOME_NAME];
-exports.XSEARCHPATH = HOME + '/src/xsearch';
+
+exports.XSEARCHPATH = config.xsearchpath;
 exports.SHAREDPATH = exports.XSEARCHPATH + '/shared';
 exports.FILETYPESPATH = exports.SHAREDPATH + '/filetypes.xml';
 exports.SEARCHOPTIONSPATH = exports.SHAREDPATH + '/searchoptions.xml';
