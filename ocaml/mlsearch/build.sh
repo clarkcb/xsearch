fa97a56683e58cfb9b6085fe44594e8bdd294ffa
Cary Clark <clarkcb@gmail.com>
2016-08-04 22:05:11 -0700

    Enhance build script for ocaml

--- ocaml/mlsearch/build.sh
+++ ocaml/mlsearch/build.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# the mlsearch executable
-echo 'corebuild -package ounit -package re2 -package xml-light -I src mlsearch.native'
-corebuild -package ounit -package re2 -package xml-light -I src mlsearch.native
 
-# unit tests
-cd tests
-TESTFILES=$(ls *test.ml)
-cd -
 
-for f in $TESTFILES; do
-	echo
-	filename="${f%.*}"
-    echo 'corebuild -package ounit -package re2 -package xml-light -I src -I tests '"$filename"'.native'
-    corebuild -package ounit -package re2 -package xml-light -I src -I tests "$filename".native
-done
+
+build_src () {
+	# the mlsearch executable
+	echo 'corebuild -package ounit -package re2 -package xml-light -I src mlsearch.native'
+	corebuild -package ounit -package re2 -package xml-light -I src mlsearch.native
+}
+
+build_tests () {
+	# unit tests
+	cd tests
+	TESTFILES=$(ls *test.ml)
+	cd -
+
+	for f in $TESTFILES; do
+		echo
+		filename="${f%.*}"
+	    echo 'corebuild -package ounit -package re2 -package xml-light -I src -I tests '"$filename"'.native'
+	    corebuild -package ounit -package re2 -package xml-light -I src -I tests "$filename".native
+	done
+}
+
+
+build_src
+
+build_tests
