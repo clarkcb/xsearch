896d586f6395f06452a6cad8b9b1a879d3718387
Cary Clark <clarkcb@gmail.com>
2016-08-04 22:07:09 -0700

    Add int_list_to_string function to common in ocaml

--- ocaml/mlsearch/src/common.ml
+++ ocaml/mlsearch/src/common.ml
@@ -11,6 +11,9 @@ let list_to_string (lst : string list) =
     | h :: t -> (rec_list_to_string (acc ^ " \"" ^ h ^ "\";") t) in
   sprintf "[%s]" (rec_list_to_string "" lst);;
 
+let int_list_to_string (lst : int list) = 
+  list_to_string (List.map lst ~f:(fun i -> sprintf "%d" i));;
+
 let regexp_list_to_string (lst : Re2.Regex.t list) = 
   let rec rec_list_to_string acc lst =
     match lst with
