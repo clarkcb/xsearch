08f6030bd7e2f4de197217818e291208e6426b08
Cary Clark <clarkcb@gmail.com>
2016-08-20 11:33:10 -0700

    Add SearchFile module/record in F#

---
+++ fsharp/FsSearch/SearchFile.fs
@@ -0,0 +1,27 @@
+﻿namespace FsSearch
+
+open System
+open System.IO
+
+module SearchFile = 
+
+    type SearchFile = {
+        containers : string list;
+        file : FileInfo;
+        filetype : FileType;
+    }
+
+    let Create (file : FileInfo) (filetype : FileType) : SearchFile =
+        {
+            containers=[];
+            file=file;
+            filetype=filetype
+        }
+
+    let ToString (sf : SearchFile) : string =
+        let container_str = 
+            match sf.containers with
+            | [] -> ""
+            | _  -> sprintf "%s!" (String.concat "!" sf.containers)
+        sprintf "%s%s" container_str (sf.file.ToString())
+;;
