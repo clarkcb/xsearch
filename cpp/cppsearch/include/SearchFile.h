d496e0d2e662ab4d717d34f740b33dfa5647d303
Cary Clark <clarkcb@gmail.com>
2017-10-08 15:54:20 -0700

    Add containers to SearchFile

--- cpp/cppsearch/include/SearchFile.h
+++ cpp/cppsearch/include/SearchFile.h
@@ -2,16 +2,24 @@
 #define CPPSEARCH_SEARCHFILE_H
 
 #include <string>
+#include <vector>
 #include "FileTypes.h"
 
 using namespace std;
 
 class SearchFile {
+private:
+    const string CONTAINER_SEPARATOR = "!";
+    void init(const vector<string>& containers, const string& path, const string& filename, FileType filetype);
+
 public:
+    vector<string> containers;
     string path;
     string filename;
     FileType filetype;
-    SearchFile(string path, string filename, FileType filetype);
+    SearchFile(string& path, string& filename, FileType filetype);
+    SearchFile(vector<string> containers, string& path, string& filename, FileType filetype);
+    SearchFile(const vector<string> &containers, const string& path, const string& filename, FileType filetype);
     string searchfile_to_string();
 };
 
--- cpp/cppsearch/src/SearchFile.cpp
+++ cpp/cppsearch/src/SearchFile.cpp
@@ -3,16 +3,36 @@
 
 using namespace std;
 
-SearchFile::SearchFile(const string p, const string f, const FileType ft) {
+SearchFile::SearchFile(string& p, string& f, FileType ft) {
+    vector<string> containers = {};
+    init(containers, p, f, ft);
+}
+
+SearchFile::SearchFile(const vector<string>& cs, const string& p, const string& f, const FileType ft) {
+    init(cs, p, f, ft);
+}
+
+SearchFile::SearchFile(vector<string> cs, string& p, string& f, FileType ft) {
+    init(cs, p, f, ft);
+}
+
+
+void SearchFile::init(const vector<string>& cs, const string& p, const string& f,
+                      const FileType ft) {
+    containers = cs;
     path = p;
     filename = f;
     filetype = ft;
 }
 
 string SearchFile::searchfile_to_string() {
+    string fullpath;
+    for (const auto& c : containers) {
+        fullpath.append(c).append(CONTAINER_SEPARATOR);
+    }
     boost::filesystem::path p(path);
     boost::filesystem::path f(filename);
     p.append(filename);
-    string fullpath = p.string();
+    fullpath.append(p.string());
     return fullpath;
 }
