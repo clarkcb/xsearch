5574fdd8b696370148b4dfd7cca1505419caa151
Cary Clark <clarkcb@gmail.com>
2018-11-10 15:18:14 -0800

    Update to latest clojure version

--- clojure/cljsearch/project.clj
+++ clojure/cljsearch/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                  [org.clojure/clojure "1.8.0"]
+                  [org.clojure/clojure "1.9.0"]
                   [org.clojure/data.json "0.2.6"]
                 ]
   :main ^:skip-aot cljsearch.cljsearch
