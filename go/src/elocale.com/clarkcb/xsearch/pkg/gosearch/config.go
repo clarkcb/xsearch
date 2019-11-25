9928d855e8b5af71db4c10720ac117ef10992de4
Cary Clark <clarkcb@gmail.com>
2019-11-25 10:49:38 -0800

    Group const together inside parens

--- go/src/elocale.com/clarkcb/xsearch/pkg/gosearch/config.go
+++ go/src/elocale.com/clarkcb/xsearch/pkg/gosearch/config.go
@@ -1,6 +1,8 @@
 package gosearch
 
-const XSEARCHPATH = "/Users/cary/src/xsearch"
-const SHAREDPATH = XSEARCHPATH + "/shared"
-const FILETYPESPATH = SHAREDPATH + "/filetypes.json"
-const SEARCHOPTIONSPATH = SHAREDPATH + "/searchoptions.json"
+const (
+	XSEARCHPATH = "/Users/cary/src/xsearch"
+	SHAREDPATH = XSEARCHPATH + "/shared"
+	FILETYPESPATH = SHAREDPATH + "/filetypes.json"
+	SEARCHOPTIONSPATH = SHAREDPATH + "/searchoptions.json"
+)
