0a7a18d8ca49e85798e24b8687f7264bf337a0cd
Cary Clark <clarkcb@gmail.com>
2020-11-07 16:51:41 -0800

    Upgrade clojure dependency versions

--- clojure/cljsearch/project.clj
+++ clojure/cljsearch/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                  [org.clojure/clojure "1.9.0"]
-                  [org.clojure/data.json "0.2.6"]
+                  [org.clojure/clojure "1.10.1"]
+                  [org.clojure/data.json "1.0.0"]
                 ]
   :main ^:skip-aot cljsearch.cljsearch
   :target-path "target/%s"
