7d3f014e4b898591902faa15c6561e789901b433
Cary Clark <clarkcb@gmail.com>
2024-12-07 02:44:14 -0800

    Add num arg processing to kotlin version

--- kotlin/ktsearch/src/main/kotlin/ktsearch/SearchOptions.kt
+++ kotlin/ktsearch/src/main/kotlin/ktsearch/SearchOptions.kt
@@ -23,6 +23,7 @@ data class SearchOption(val shortArg: String?, val longArg: String, val desc: St
 class SearchOptions {
     private val searchOptionsJsonPath = "/searchoptions.json"
     private val searchOptions : List<SearchOption>
+    private var longArgMap = mutableMapOf<String, String>()
 
     init {
         searchOptions = loadSearchOptionsFromJson()
@@ -36,9 +37,12 @@ class SearchOptions {
         while (searchOptionsArray.hasNext()) {
             val searchOptionObj = searchOptionsArray.next() as JSONObject
             val longArg = searchOptionObj.getString("long")
+            longArgMap[longArg] = longArg
             val shortArg =
                 if (searchOptionObj.has("short")) {
-                    searchOptionObj.getString("short")
+                    val sArg = searchOptionObj.getString("short")
+                    longArgMap[sArg] = longArg
+                    sArg
                 } else {
                     null
                 }
@@ -48,84 +52,18 @@ class SearchOptions {
         return options.toList().sortedBy { it.sortArg }
     }
 
-    private fun getArgMap() : Map<String, String> {
-        val longOpts = searchOptions.map { Pair(it.longArg, it.longArg) }.toMap()
-        val shortOpts = searchOptions.filter { it.shortArg != null }.map { Pair(it.shortArg!!, it.longArg) }.toMap()
-        return longOpts.plus(shortOpts)
-    }
-
-    private val argActionMap: Map<String, ((String, SearchSettings) -> SearchSettings)> = mapOf(
-        "encoding" to
-                { s, ss -> ss.copy(textFileEncoding = s) },
-        "in-archiveext" to
-                { s, ss -> ss.copy(inArchiveExtensions = addExtensions(s, ss.inArchiveExtensions)) },
-        "in-archivefilepattern" to
-                { s, ss -> ss.copy(inArchiveFilePatterns = ss.inArchiveFilePatterns.plus(Regex(s))) },
-        "in-dirpattern" to
-                { s, ss -> ss.copy(inDirPatterns = ss.inDirPatterns.plus(Regex(s))) },
-        "in-ext" to
-                { s, ss -> ss.copy(inExtensions = addExtensions(s, ss.inExtensions)) },
-        "in-filepattern" to
-                { s, ss -> ss.copy(inFilePatterns = ss.inFilePatterns.plus(Regex(s))) },
-        "in-filetype" to
-                { s, ss -> ss.copy(inFileTypes = addFileTypes(s, ss.inFileTypes)) },
-        "in-linesafterpattern" to
-                { s, ss -> ss.copy(inLinesAfterPatterns = ss.inLinesAfterPatterns.plus(Regex(s))) },
-        "in-linesbeforepattern" to
-                { s, ss -> ss.copy(inLinesBeforePatterns = ss.inLinesBeforePatterns.plus(Regex(s))) },
-        "linesafter" to
-                { s, ss -> ss.copy(linesAfter = s.toInt()) },
-        "linesaftertopattern" to
-                { s, ss -> ss.copy(linesAfterToPatterns = ss.linesAfterToPatterns.plus(Regex(s))) },
-        "linesafteruntilpattern" to
-                { s, ss -> ss.copy(linesAfterUntilPatterns = ss.linesAfterUntilPatterns.plus(Regex(s))) },
-        "linesbefore" to
-                { s, ss -> ss.copy(linesBefore = s.toInt()) },
-        "maxdepth" to
-                { s, ss -> ss.copy(maxDepth = Integer.parseInt(s)) },
-        "maxlastmod" to
-                { s, ss -> ss.copy(maxLastMod = getLastModFromString(s)) },
-        "maxlinelength" to
-                { s, ss -> ss.copy(maxLineLength = s.toInt()) },
-        "maxsize" to
-                { s, ss -> ss.copy(maxSize = Integer.parseInt(s)) },
-        "mindepth" to
-                { s, ss -> ss.copy(minDepth = Integer.parseInt(s)) },
-        "minlastmod" to
-                { s, ss -> ss.copy(minLastMod = getLastModFromString(s)) },
-        "minsize" to
-                { s, ss -> ss.copy(minSize = Integer.parseInt(s)) },
-        "out-archiveext" to
-                { s, ss -> ss.copy(outArchiveExtensions = addExtensions(s, ss.outArchiveExtensions)) },
-        "out-archivefilepattern" to
-                { s, ss -> ss.copy(outArchiveFilePatterns = ss.outArchiveFilePatterns.plus(Regex(s))) },
-        "out-dirpattern" to
-                { s, ss -> ss.copy(outDirPatterns = ss.outDirPatterns.plus(Regex(s))) },
-        "out-ext" to
-                { s, ss -> ss.copy(outExtensions = addExtensions(s, ss.outExtensions)) },
-        "out-filepattern" to
-                { s, ss -> ss.copy(outFilePatterns = ss.outFilePatterns.plus(Regex(s))) },
-        "out-filetype" to
-                { s, ss -> ss.copy(outFileTypes = addFileTypes(s, ss.outFileTypes)) },
-        "out-linesafterpattern" to
-                { s, ss -> ss.copy(outLinesAfterPatterns = ss.outLinesAfterPatterns.plus(Regex(s))) },
-        "out-linesbeforepattern" to
-                { s, ss -> ss.copy(outLinesBeforePatterns = ss.outLinesBeforePatterns.plus(Regex(s))) },
-        "searchpattern" to
-                { s, ss -> ss.copy(searchPatterns = ss.searchPatterns.plus(Regex(s))) },
-        "settings-file" to
-                { s, ss -> settingsFromFile(s, ss) },
-        "sort-by" to
-                { s, ss -> ss.copy(sortBy = SortBy.forName(s)) },
-    )
-
-    private val boolFlagActionMap: Map<String, ((Boolean, SearchSettings) -> SearchSettings)> = mapOf(
-        "archivesonly" to { b, ss -> if (b) ss.copy(archivesOnly = b,
-            searchArchives = b) else ss.copy(archivesOnly = b) },
+    private val boolActionMap: Map<String, ((Boolean, SearchSettings) -> SearchSettings)> = mapOf(
+        "archivesonly" to { b, ss ->
+            if (b) ss.copy(
+                archivesOnly = b,
+                searchArchives = b
+            ) else ss.copy(archivesOnly = b) },
         "allmatches" to { b, ss -> ss.copy(firstMatch = !b) },
         "colorize" to { b, ss -> ss.copy(colorize = b) },
-        "debug" to { b, ss -> if (b) ss.copy(debug = b, verbose = b) else
-            ss.copy(debug = b) },
+        "debug" to { b, ss ->
+            if (b) ss.copy(debug = b, verbose = b) else
+                ss.copy(debug = b)
+        },
         "excludehidden" to { b, ss -> ss.copy(includeHidden = !b) },
         "firstmatch" to { b, ss -> ss.copy(firstMatch = b) },
         "followsymlinks" to { b, ss -> ss.copy(followSymlinks = b) },
@@ -155,14 +93,78 @@ class SearchOptions {
         "version" to { b, ss -> ss.copy(printVersion = b) }
     )
 
+    private val stringActionMap: Map<String, ((String, SearchSettings) -> SearchSettings)> = mapOf(
+        "encoding" to
+                { s, ss -> ss.copy(textFileEncoding = s) },
+        "in-archiveext" to
+                { s, ss -> ss.copy(inArchiveExtensions = addExtensions(s, ss.inArchiveExtensions)) },
+        "in-archivefilepattern" to
+                { s, ss -> ss.copy(inArchiveFilePatterns = ss.inArchiveFilePatterns.plus(Regex(s))) },
+        "in-dirpattern" to
+                { s, ss -> ss.copy(inDirPatterns = ss.inDirPatterns.plus(Regex(s))) },
+        "in-ext" to
+                { s, ss -> ss.copy(inExtensions = addExtensions(s, ss.inExtensions)) },
+        "in-filepattern" to
+                { s, ss -> ss.copy(inFilePatterns = ss.inFilePatterns.plus(Regex(s))) },
+        "in-filetype" to
+                { s, ss -> ss.copy(inFileTypes = addFileTypes(s, ss.inFileTypes)) },
+        "in-linesafterpattern" to
+                { s, ss -> ss.copy(inLinesAfterPatterns = ss.inLinesAfterPatterns.plus(Regex(s))) },
+        "in-linesbeforepattern" to
+                { s, ss -> ss.copy(inLinesBeforePatterns = ss.inLinesBeforePatterns.plus(Regex(s))) },
+        "linesaftertopattern" to
+                { s, ss -> ss.copy(linesAfterToPatterns = ss.linesAfterToPatterns.plus(Regex(s))) },
+        "linesafteruntilpattern" to
+                { s, ss -> ss.copy(linesAfterUntilPatterns = ss.linesAfterUntilPatterns.plus(Regex(s))) },
+        "maxlastmod" to
+                { s, ss -> ss.copy(maxLastMod = getLastModFromString(s)) },
+        "minlastmod" to
+                { s, ss -> ss.copy(minLastMod = getLastModFromString(s)) },
+        "out-archiveext" to
+                { s, ss -> ss.copy(outArchiveExtensions = addExtensions(s, ss.outArchiveExtensions)) },
+        "out-archivefilepattern" to
+                { s, ss -> ss.copy(outArchiveFilePatterns = ss.outArchiveFilePatterns.plus(Regex(s))) },
+        "out-dirpattern" to
+                { s, ss -> ss.copy(outDirPatterns = ss.outDirPatterns.plus(Regex(s))) },
+        "out-ext" to
+                { s, ss -> ss.copy(outExtensions = addExtensions(s, ss.outExtensions)) },
+        "out-filepattern" to
+                { s, ss -> ss.copy(outFilePatterns = ss.outFilePatterns.plus(Regex(s))) },
+        "out-filetype" to
+                { s, ss -> ss.copy(outFileTypes = addFileTypes(s, ss.outFileTypes)) },
+        "out-linesafterpattern" to
+                { s, ss -> ss.copy(outLinesAfterPatterns = ss.outLinesAfterPatterns.plus(Regex(s))) },
+        "out-linesbeforepattern" to
+                { s, ss -> ss.copy(outLinesBeforePatterns = ss.outLinesBeforePatterns.plus(Regex(s))) },
+        "searchpattern" to
+                { s, ss -> ss.copy(searchPatterns = ss.searchPatterns.plus(Regex(s))) },
+        "settings-file" to
+                { s, ss -> settingsFromFile(s, ss) },
+        "sort-by" to
+                { s, ss -> ss.copy(sortBy = SortBy.forName(s)) },
+    )
+
+    private val intActionMap: Map<String, ((Int, SearchSettings) -> SearchSettings)> = mapOf(
+        "linesafter" to { i, ss -> ss.copy(linesAfter = i) },
+        "linesbefore" to { i, ss -> ss.copy(linesBefore = i) },
+        "maxdepth" to { i, ss -> ss.copy(maxDepth = i) },
+        "maxlinelength" to { i, ss -> ss.copy(maxLineLength = i) },
+        "mindepth" to { i, ss -> ss.copy(minDepth = i) },
+    )
+
+    private val longActionMap: Map<String, ((Long, SearchSettings) -> SearchSettings)> = mapOf(
+        "maxsize" to { l, ss -> ss.copy(maxSize = l) },
+        "minsize" to { l, ss -> ss.copy(minSize = l) },
+    )
+
     private fun settingsFromFile(filePath: String, settings: SearchSettings) : SearchSettings {
         val file = File(filePath)
         try {
             val json = file.readText()
             return settingsFromJson(json, settings)
-        } catch (e: FileNotFoundException) {
+        } catch (_: FileNotFoundException) {
             throw SearchException("Settings file not found: $filePath")
-        } catch (e: IOException) {
+        } catch (_: IOException) {
             throw SearchException("IOException reading settings file: $filePath")
         }
     }
@@ -186,20 +188,20 @@ class SearchOptions {
 
     private fun applySetting(key: String, obj: Any, settings: SearchSettings): SearchSettings {
         when (obj) {
-            is String -> {
-                return applySetting(key, obj, settings)
-            }
             is Boolean -> {
-                return applySetting(key, obj, settings)
+                return applyBoolSetting(key, obj, settings)
+            }
+            is String -> {
+                return applyStringSetting(key, obj, settings)
             }
             is Int -> {
-                return applySetting(key, obj.toString(), settings)
+                return applyIntSetting(key, obj, settings)
             }
             is Long -> {
-                return applySetting(key, obj.toString(), settings)
+                return applyLongSetting(key, obj, settings)
             }
             is JSONArray -> {
-                return applySetting(key, obj.toList().map { it as String }, settings)
+                return applySettings(key, obj.toList().map { it as String }, settings)
             }
             else -> {
                 return settings
@@ -207,13 +209,18 @@ class SearchOptions {
         }
     }
 
-    private fun applySetting(key: String, s: String, settings: SearchSettings): SearchSettings {
+    private fun applyBoolSetting(key: String, bool: Boolean, settings: SearchSettings): SearchSettings {
+        if (this.boolActionMap.containsKey(key)) {
+            return this.boolActionMap[key]!!.invoke(bool, settings)
+        } else {
+            throw SearchException("Invalid option: $key")
+        }
+    }
+
+    private fun applyStringSetting(key: String, s: String, settings: SearchSettings): SearchSettings {
         return when {
-            this.argActionMap.containsKey(key) -> {
-                this.argActionMap[key]!!.invoke(s, settings)
-            }
-            key == "path" -> {
-                settings.copy(paths = settings.paths.plus(s))
+            this.stringActionMap.containsKey(key) -> {
+                this.stringActionMap[key]!!.invoke(s, settings)
             }
             else -> {
                 throw SearchException("Invalid option: $key")
@@ -221,43 +228,61 @@ class SearchOptions {
         }
     }
 
-    private fun applySetting(key: String, bool: Boolean, settings: SearchSettings): SearchSettings {
-        if (this.boolFlagActionMap.containsKey(key)) {
-            return this.boolFlagActionMap[key]!!.invoke(bool, settings)
+    private fun applyIntSetting(key: String, i: Int, settings: SearchSettings): SearchSettings {
+        return if (this.intActionMap.containsKey(key)) {
+            this.intActionMap[key]!!.invoke(i, settings)
+        } else if (this.longActionMap.containsKey(key)) {
+            this.longActionMap[key]!!.invoke(i.toLong(), settings)
         } else {
-            throw SearchException("Invalid option: $key")
+            throw FindException("Invalid option: $key")
         }
     }
 
-    private fun applySetting(key: String, lst: List<String>, settings: SearchSettings): SearchSettings {
+    private fun applyLongSetting(key: String, l: Long, settings: SearchSettings): SearchSettings {
+        return if (this.intActionMap.containsKey(key)) {
+            this.intActionMap[key]!!.invoke(l.toInt(), settings)
+        } else if (this.longActionMap.containsKey(key)) {
+            this.longActionMap[key]!!.invoke(l, settings)
+        } else {
+            throw FindException("Invalid option: $key")
+        }
+    }
+
+    private fun applySettings(key: String, lst: List<String>, settings: SearchSettings): SearchSettings {
         return if (lst.isEmpty()) settings
         else {
-            applySetting(key, lst.drop(1), applySetting(key, lst.first(), settings))
+            applySettings(key, lst.drop(1), applyStringSetting(key, lst.first(), settings))
         }
     }
 
     fun settingsFromArgs(args : Array<String>) : SearchSettings {
-        val argMap = getArgMap()
-        fun recSettingsFromArgs(args: List<String>, settings: SearchSettings) : SearchSettings {
+        fun recSettingsFromArgs(args: List<String>, settings: SearchSettings): SearchSettings {
             if (args.isEmpty()) return settings
             val nextArg = args.first()
             if (nextArg.startsWith("-")) {
                 val arg = nextArg.dropWhile { it == '-' }
-                if (argMap.containsKey(arg)) {
-                    val longArg = argMap[arg]
-                    return if (argActionMap.containsKey(longArg)) {
-                        if (args.size > 1) {
-                            val argVal = args.drop(1).first()
-                            val ss = argActionMap[longArg]!!.invoke(argVal, settings)
-                            recSettingsFromArgs(args.drop(2), ss)
+                val longArg = longArgMap[arg]
+                return if (boolActionMap.containsKey(arg)) {
+                    val ss = boolActionMap[longArg]!!.invoke(true, settings)
+                    recSettingsFromArgs(args.drop(1), ss)
+                } else if (stringActionMap.containsKey(longArg)
+                    || intActionMap.containsKey(longArg)
+                    || longActionMap.containsKey(longArg)
+                ) {
+                    if (args.size > 1) {
+                        val argVal = args.drop(1).first()
+                        val ss = if (stringActionMap.containsKey(longArg)) {
+                            stringActionMap[longArg]!!.invoke(argVal, settings)
+                        } else if (intActionMap.containsKey(longArg)) {
+                            intActionMap[longArg]!!.invoke(argVal.toInt(), settings)
+                        } else if (longActionMap.containsKey(longArg)) {
+                            longActionMap[longArg]!!.invoke(argVal.toLong(), settings)
                         } else {
-                            throw SearchException("Missing value for option $arg")
+                            throw FindException("Unhandled option $arg")
                         }
-                    } else if (boolFlagActionMap.containsKey(longArg)) {
-                        val ss = boolFlagActionMap[longArg]!!.invoke(true, settings)
-                        recSettingsFromArgs(args.drop(1), ss)
+                        recSettingsFromArgs(args.drop(2), ss)
                     } else {
-                        throw SearchException("Invalid option: $arg")
+                        throw SearchException("Missing value for option $arg")
                     }
                 } else {
                     throw SearchException("Invalid option: $arg")
@@ -266,6 +291,7 @@ class SearchOptions {
                 return recSettingsFromArgs(args.drop(1), settings.copy(paths = settings.paths.plus(nextArg)))
             }
         }
+        // default printResults to true since running as cli
         return recSettingsFromArgs(args.toList(), getDefaultSettings().copy(printResults = true))
     }
 
@@ -273,7 +299,7 @@ class SearchOptions {
         log(getUsageString())
     }
 
-    private fun getUsageString() : String {
+    private fun getUsageString(): String {
         val sb = StringBuilder()
         sb.append("Usage:\n")
         sb.append(" ktsearch [options] -s <searchpattern> <path> [<path> ...]\n\n")
@@ -281,8 +307,9 @@ class SearchOptions {
         fun getOptString(so: SearchOption): String {
             return (if (so.shortArg == null) "" else "-${so.shortArg},") + "--${so.longArg}"
         }
+
         val optPairs = searchOptions.map { Pair(getOptString(it), it.desc) }
-        val longest = optPairs.map { it.first.length }.maxOrNull()
+        val longest = optPairs.maxOfOrNull { it.first.length }
         val format = " %1${'$'}-${longest}s  %2${'$'}s\n"
         for (o in optPairs) {
             sb.append(String.format(format, o.first, o.second))
