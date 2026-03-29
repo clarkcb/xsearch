4bffceee6e04b5900ad95035b9379db39911d31a
Cary Clark <clarkcb@gmail.com>
2026-03-29 11:15:35 -0700

    Inherit to_string from FindSettings in perl version

--- perl/plsearch/lib/plsearch/SearchSettings.pm
+++ perl/plsearch/lib/plsearch/SearchSettings.pm
@@ -48,64 +48,6 @@ sub new {
     return $self;
 }
 
-sub to_string {
-    my $self = shift @_;
-    my $s = "SearchSettings(" .
-        'archives_only=' . plfind::common::bool_to_string($self->{archives_only}) .
-        ', colorize=' . plfind::common::bool_to_string($self->{colorize}) .
-        ', debug=' . plfind::common::bool_to_string($self->{debug}) .
-        ', first_match=' . plfind::common::bool_to_string($self->{first_match}) .
-        ', follow_symlinks=' . plfind::common::bool_to_string($self->{follow_symlinks}) .
-        ', in_archive_extensions=' . plfind::common::strings_aref_to_string($self->{in_archive_extensions}) .
-        ', in_archive_file_patterns=' . plfind::common::strings_aref_to_string($self->{in_archive_file_patterns}) .
-        ', include_hidden=' . plfind::common::bool_to_string($self->{include_hidden}) .
-        ', in_dir_patterns=' . plfind::common::strings_aref_to_string($self->{in_dir_patterns}) .
-        ', in_extensions=' . plfind::common::strings_aref_to_string($self->{in_extensions}) .
-        ', in_file_patterns=' . plfind::common::strings_aref_to_string($self->{in_file_patterns}) .
-        ', in_file_types=' . plfind::common::file_types_aref_to_string($self->{in_file_types}) .
-        ', in_lines_after_patterns=' . plfind::common::strings_aref_to_string($self->{in_lines_after_patterns}) .
-        ', in_lines_before_patterns=' . plfind::common::strings_aref_to_string($self->{in_lines_before_patterns}) .
-        ', lines_after=' . $self->{lines_after} .
-        ', lines_after_to_patterns=' . plfind::common::strings_aref_to_string($self->{lines_after_to_patterns}) .
-        ', lines_after_until_patterns=' . plfind::common::strings_aref_to_string($self->{lines_after_until_patterns}) .
-        ', lines_before=' . $self->{lines_before} .
-        ', max_depth=' . $self->{max_depth} .
-        ', max_last_mod=' . plfind::common::datetime_to_string($self->{max_last_mod}) .
-        ', max_line_length=' . $self->{max_line_length} .
-        ', max_size=' . $self->{max_size} .
-        ', min_depth=' . $self->{min_depth} .
-        ', min_last_mod=' . plfind::common::datetime_to_string($self->{min_last_mod}) .
-        ', min_size=' . $self->{min_size} .
-        ', multi_line_search=' . plfind::common::bool_to_string($self->{multi_line_search}) .
-        ', out_archive_extensions=' . plfind::common::strings_aref_to_string($self->{out_archive_extensions}) .
-        ', out_archive_file_patterns=' . plfind::common::strings_aref_to_string($self->{out_archive_file_patterns}) .
-        ', out_dir_patterns=' . plfind::common::strings_aref_to_string($self->{out_dir_patterns}) .
-        ', out_extensions=' . plfind::common::strings_aref_to_string($self->{out_extensions}) .
-        ', out_file_patterns=' . plfind::common::strings_aref_to_string($self->{out_file_patterns}) .
-        ', out_file_types=' . plfind::common::file_types_aref_to_string($self->{out_file_types}) .
-        ', out_lines_after_patterns=' . plfind::common::strings_aref_to_string($self->{out_lines_after_patterns}) .
-        ', out_lines_before_patterns=' . plfind::common::strings_aref_to_string($self->{out_lines_before_patterns}) .
-        ', paths=' . plfind::common::strings_aref_to_string($self->{paths}) .
-        ', print_dirs=' . plfind::common::bool_to_string($self->{print_dirs}) .
-        ', print_files=' . plfind::common::bool_to_string($self->{print_files}) .
-        ', print_lines=' . plfind::common::bool_to_string($self->{print_lines}) .
-        ', print_matches=' . plfind::common::bool_to_string($self->{print_matches}) .
-        ', print_results=' . plfind::common::bool_to_string($self->{print_results}) .
-        ', print_usage=' . plfind::common::bool_to_string($self->{print_usage}) .
-        ', print_version=' . plfind::common::bool_to_string($self->{print_version}) .
-        ', recursive=' . plfind::common::bool_to_string($self->{recursive}) .
-        ', search_archives=' . plfind::common::bool_to_string($self->{search_archives}) .
-        ', search_patterns=' . plfind::common::strings_aref_to_string($self->{search_patterns}) .
-        ', sort_by=' . $self->{sort_by} .
-        ', sort_case_insensitive=' . plfind::common::bool_to_string($self->{sort_case_insensitive}) .
-        ', sort_descending=' . plfind::common::bool_to_string($self->{sort_descending}) .
-        ', text_file_encoding="' . $self->{text_file_encoding} . '"' .
-        ', unique_lines=' . plfind::common::bool_to_string($self->{unique_lines}) .
-        ', verbose=' . plfind::common::bool_to_string($self->{verbose}) .
-        ')';
-    return $s;
-}
-
 1;
 
 __END__
