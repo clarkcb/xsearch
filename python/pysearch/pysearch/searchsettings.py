f2d517d56d96e1cbcbf8e1f0d416d51e5cfc65e4
Cary Clark <clarkcb@gmail.com>
2026-09-07 17:02:43 -0700

    Typing improvements to python version

--- python/pysearch/pysearch/searchsettings.py
+++ python/pysearch/pysearch/searchsettings.py
@@ -9,33 +9,32 @@
 ###############################################################################
 """
 import re
+from collections.abc import Collection
 from datetime import datetime
 from pathlib import Path
-from typing import Optional, Pattern
+from typing import Pattern
 
 from pyfind import Color, FindSettings, FindException, SortBy, FileType
 
 from .searchexception import SearchException
 
 PatternSet = set[Pattern]
+StringOrStringColl = str | Collection[str]
+PatternOrPatternColl = Pattern | Collection[Pattern]
+FileTypeOrFileTypeColl = FileType | Collection[FileType]
+PathOrPathColl = Path | Collection[Path]
 
 
 class SearchSettings(FindSettings):
     """a class to encapsulate search settings for a particular search session"""
 
-    __slots__ = FindSettings.__slots__ + [
+    __slots__ = [
         'first_match', 'in_lines_after_patterns', 'in_lines_before_patterns',
         'line_color', 'lines_after', 'lines_after_to_patterns', 'lines_after_until_patterns',
         'lines_before', 'max_line_length', 'multi_line_search', 'out_lines_after_patterns',
         'out_lines_before_patterns', 'print_lines', 'print_matches', 'print_results',
         'search_archives', 'search_patterns', 'text_file_encoding', 'unique_lines'
     ]
-
-    def add_strs_to_set(self, strs: list[str] | set[str] | str, set_name: str):
-        try:
-            FindSettings.add_strs_to_set(self, strs, set_name)
-        except FindException as e:
-            raise SearchException(str(e))
 
     def __init__(self,
                  archives_only: bool = False,
@@ -47,37 +46,37 @@ class SearchSettings(FindSettings):
                  file_color: Color = Color.MAGENTA,
                  first_match: bool = False,
                  follow_symlinks: bool = False,
-                 in_archive_extensions: list[str] | set[str] | str = None,
-                 in_archive_file_patterns: list | set | str | Pattern = None,
+                 in_archive_extensions: StringOrStringColl | None = None,
+                 in_archive_file_patterns: PatternOrPatternColl | None = None,
                  include_hidden: bool = False,
-                 in_dir_patterns: list | set | str | Pattern = None,
-                 in_extensions: list[str] | set[str] | str = None,
-                 in_file_patterns: list | set | str | Pattern = None,
-                 in_file_types: list | set | str | FileType = None,
-                 in_lines_after_patterns: PatternSet = None,
-                 in_lines_before_patterns: PatternSet = None,
+                 in_dir_patterns: PatternOrPatternColl | None = None,
+                 in_extensions: StringOrStringColl | None = None,
+                 in_file_patterns: PatternOrPatternColl | None = None,
+                 in_file_types: FileTypeOrFileTypeColl | None = None,
+                 in_lines_after_patterns: PatternSet | None = None,
+                 in_lines_before_patterns: PatternSet | None = None,
                  line_color: Color = Color.GREEN,
                  lines_after: int = 0,
-                 lines_after_to_patterns: PatternSet = None,
-                 lines_after_until_patterns: PatternSet = None,
+                 lines_after_to_patterns: PatternSet | None = None,
+                 lines_after_until_patterns: PatternSet | None = None,
                  lines_before: int = 0,
                  max_depth: int = -1,
-                 max_last_mod: Optional[datetime] = None,
+                 max_last_mod: datetime | None = None,
                  max_line_length: int = 150,
                  max_size: int = 0,
                  min_depth: int = -1,
-                 min_last_mod: Optional[datetime] = None,
+                 min_last_mod: datetime | None = None,
                  min_size: int = 0,
                  multi_line_search: bool = False,
-                 out_archive_extensions: list[str] | set[str] | str = None,
-                 out_archive_file_patterns: list | set | str | Pattern = None,
-                 out_dir_patterns: list | set | str | Pattern = None,
-                 out_extensions: list[str] | set[str] | str = None,
-                 out_file_patterns: list | set | str | Pattern = None,
-                 out_file_types: list | set | str | FileType = None,
-                 out_lines_after_patterns: PatternSet = None,
-                 out_lines_before_patterns: PatternSet = None,
-                 paths: list[Path] | set[Path] | list[str] | set[str] | str = None,
+                 out_archive_extensions: StringOrStringColl | None = None,
+                 out_archive_file_patterns: PatternOrPatternColl | None = None,
+                 out_dir_patterns: PatternOrPatternColl | None = None,
+                 out_extensions: StringOrStringColl | None = None,
+                 out_file_patterns: PatternOrPatternColl | None = None,
+                 out_file_types: FileTypeOrFileTypeColl | None = None,
+                 out_lines_after_patterns: PatternSet | None = None,
+                 out_lines_before_patterns: PatternSet | None = None,
+                 paths: PathOrPathColl | StringOrStringColl | None = None,
                  print_dirs: bool = False,
                  print_files: bool = False,
                  print_lines: bool = False,
@@ -87,7 +86,7 @@ class SearchSettings(FindSettings):
                  print_version: bool = False,
                  recursive: bool = True,
                  search_archives: bool = False,
-                 search_patterns: PatternSet = None,
+                 search_patterns: PatternSet | None = None,
                  sort_by: SortBy = SortBy.FILEPATH,
                  sort_case_insensitive: bool = False,
                  sort_descending: bool = False,
@@ -160,7 +159,13 @@ class SearchSettings(FindSettings):
         self.text_file_encoding = text_file_encoding
         self.unique_lines = unique_lines
 
-    def add_patterns(self, patterns: list | set | str | Pattern, pattern_set_name: str, compile_flag=re.S | re.U):
+    def add_strs_to_set(self, strs: StringOrStringColl, set_name: str):
+        try:
+            FindSettings.add_strs_to_set(self, strs, set_name)
+        except FindException as e:
+            raise SearchException(str(e))
+
+    def add_patterns(self, patterns: PatternOrPatternColl | StringOrStringColl, pattern_set_name: str, compile_flag=re.S | re.U):
         try:
             FindSettings.add_patterns(self, patterns, pattern_set_name, compile_flag)
         except FindException as e:
